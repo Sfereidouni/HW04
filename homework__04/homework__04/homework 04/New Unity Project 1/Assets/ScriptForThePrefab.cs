ec893a267b3f4c32dbb4ef616eeae394073bb9ab
Sfereidouni <s.fereidouni.17@ucl.ac.uk>
2018-03-18 18:45:19 +0000

    Homework04

---
+++ homework__04/homework__04/homework 04/New Unity Project 1/Assets/ScriptForThePrefab.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptForThePrefab : MonoBehaviour {
+
+    GameObject[] CubeArray;
+    IEnumerator CreateArray;
+    public Material cubeMaterial;
+    public Camera camera;
+
+	void Start () 
+    {
+        StartCoroutine(ChangeColor());
+	}
+    void Update()
+    {
+        RaycastHit objectInFront;
+       
+        Vector3 castedRayDirection = transform.TransformDirection(Vector3.forward);
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 10000))
+
+        {
+            string objectInFrontName = hit.transform.name;
+            Debug.Log("There is an object in front of me! It's name is" + objectInFrontName);
+
+            Destroy(hit.transform.gameObject);
+
+        }
+      
+
+    }
+
+    IEnumerator ChangeColor()
+    {
+        if (cubeMaterial.color != Color.grey){
+            cubeMaterial.color = Color.grey;
+        }
+        else {
+                cubeMaterial.color = Color.red;
+        }
+        yield return new WaitForSeconds(0.6f);
+    }
+
+}
---
+++ homework__04/homework__04/homework 04/New Unity Project 1/Assets/Session04_Homework.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Session04_Homework : MonoBehaviour 
+{
+    public GameObject CubePrefab;
+    IEnumerator MakeArray;
+
+    void Start()
+    {
+        MakeArray = MakeArrayOfCubes(); 
+        StartCoroutine(MakeArray);
+    }
+
+
+
+    IEnumerator MakeArrayOfCubes()
+    {
+        while (Time.time < 30)
+        {
+            Vector3 position = new Vector3(1.5f * Time.time, 2.5f * Time.time, 1.85f * Time.time);
+            Quaternion rotation = new Quaternion(0, 0, 0, 1);
+            GameObject newCubeArray = Instantiate(CubePrefab, position, rotation);
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
